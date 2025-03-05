a260aa897c5cd1e6e21520c77dd6d2646553e158
Ega1232387 <>
2025-03-05 19:04:56 +0300

    Last Sync: 2025-03-05 16:04:56 (Mobile)

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
+    private int health;
+    private Ability[] abilities; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void TakeDamage (int damage)
     {
-        
+        if(damage < 0) return;
+        this.health -= damage;
+        health = Mathf.Clamp(0, this.health);
+        if(this.health == 0) Death();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Death()
     {
-        
+        Debug.Log("Вы умерли(")
     }
 }
